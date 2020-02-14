cd50e41257a2caadcf891cf628ab9a473ac3edbc
Jungtaek Lim (HeartSaVioR) <kabhwan.opensource@gmail.com>
2020-02-14 15:46:17 +0900

    Print arguments to Kafka data source query file

--- src/kafka_data_source.py
+++ src/kafka_data_source.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     input_kafka_bootstrap_servers = sys.argv[1]
     input_kafka_subscribe = sys.argv[2]
-    input_kafka_startingOffsets = sys.argv[3]
+    input_kafka_starting_offsets = sys.argv[3]
 
     output_kafka_bootstrap_servers = sys.argv[4]
     output_kafka_topic = sys.argv[5]
@@ -21,6 +21,17 @@ if __name__ == "__main__":
     checkpoint_path = sys.argv[6]
     trigger_interval_secs = int(sys.argv[7])
     output_partition_num = int(sys.argv[8])
+
+    print("=" * 40)
+    print("Input Kafka bootstrap servers: %s" % input_kafka_bootstrap_servers)
+    print("Input kafka topic to subscribe: %s" % input_kafka_subscribe)
+    print("Input Kafka starting offsets: %s" % input_kafka_starting_offsets)
+    print("Output Kafka bootstrap servers: %s" % output_kafka_bootstrap_servers)
+    print("Output Kafka topic: %s" % output_kafka_topic)
+    print("Checkpoint path: %s" % checkpoint_path)
+    print("Trigger interval: %s" % trigger_interval_secs)
+    print("Number of output partitions: %s" % output_partition_num)
+    print("=" * 40)
 
     spark = SparkSession \
         .builder \
@@ -32,7 +43,7 @@ if __name__ == "__main__":
         .format("kafka") \
         .option("kafka.bootstrap.servers", input_kafka_bootstrap_servers) \
         .option("subscribe", input_kafka_subscribe) \
-        .option("startingOffsets", input_kafka_startingOffsets) \
+        .option("startingOffsets", input_kafka_starting_offsets) \
         .option("failOnDataLoss", "false") \
         .load()
 
