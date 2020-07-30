7ee6f769106f09f5bb7b46aca0f768f32fe51789
Jungtaek Lim (HeartSaVioR) <kabhwan.opensource@gmail.com>
2020-07-31 08:54:53 +0900

    Separate source file for hadoop catalog vs hive catalog on Iceberg (still only on rate source)

--- src/rate_data_source_to_iceberg_hive_catalog.py
+++ src/rate_data_source_to_iceberg_hive_catalog.py
@@ -5,23 +5,25 @@ from pyspark.sql import SparkSession
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) < 6:
-        print("USAGE: %s [input_records_per_second] [iceberg_warehouse_path] [table_name] " +
+    if len(sys.argv) < 7:
+        print("USAGE: %s [input_records_per_second] [hive_metastore_uris] [table_location] [table_name] " +
               "[checkpoint_path] [trigger_interval_secs] [output_partition_num]")
         sys.exit(1)
 
     input_records_per_second = sys.argv[1]
 
-    iceberg_warehouse_path = sys.argv[2]
-    table_name = sys.argv[3]
+    hive_metastore_uris = sys.argv[2]
+    table_location = sys.argv[3]
+    table_name = sys.argv[4]
 
-    checkpoint_path = sys.argv[4]
-    trigger_interval_secs = int(sys.argv[5])
-    output_partition_num = int(sys.argv[6])
+    checkpoint_path = sys.argv[5]
+    trigger_interval_secs = int(sys.argv[6])
+    output_partition_num = int(sys.argv[7])
 
     print("=" * 40)
     print("Input records per second: %s" % input_records_per_second)
-    print("Iceberg warehouse path: %s" % iceberg_warehouse_path)
+    print("Hive metastore thrift URIs: %s" % hive_metastore_uris)
+    print("Iceberg table location: %s" % table_location)
     print("Iceberg table name: %s" % table_name)
     print("Checkpoint path: %s" % checkpoint_path)
     print("Trigger interval: %s" % trigger_interval_secs)
@@ -30,10 +32,10 @@ if __name__ == "__main__":
 
     spark = SparkSession \
         .builder \
-        .appName("SSRateDataSourceToIcebergOnHadoopCatalog") \
+        .appName("SSRateDataSourceToIcebergOnHiveCatalog") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.iceberg.spark.SparkSessionCatalog") \
-        .config("spark.sql.catalog.spark_catalog.type", "hadoop") \
-        .config("spark.sql.catalog.spark_catalog.warehouse", iceberg_warehouse_path) \
+        .config("spark.sql.catalog.spark_catalog.type", "hive") \
+        .config("spark.sql.catalog.spark_catalog.uri", hive_metastore_uris) \
         .getOrCreate()
 
     spark.sql("""
@@ -42,8 +44,9 @@ if __name__ == "__main__":
             `value` long COMMENT 'content'
         )
         USING iceberg
+        LOCATION '{}'
         TBLPROPERTIES ('write.metadata.delete-after-commit.enabled'='true')
-        """.format(table_name))
+        """.format(table_name, table_location))
 
     df = spark \
         .readStream \
@@ -60,9 +63,9 @@ if __name__ == "__main__":
         .format("iceberg") \
         .outputMode("append") \
         .trigger(processingTime="%d seconds" % trigger_interval_secs) \
-        .option("path", iceberg_warehouse_path + "/default/" + table_name) \
+        .option("path", "default." + table_name) \
         .option("checkpointLocation", checkpoint_path) \
-        .start(queryName='SS-rate-datasource-iceberg-sink-hadoop-catalog-experiment')
+        .start(queryName='SS-rate-datasource-iceberg-sink-hive-catalog-experiment')
 
     query.awaitTermination()
 
